3766152855cd650a4a513ff1c7b925b576e40a79
HAE2NI <hae0_02ni@ewhain.net>
2023-11-11 22:03:01 +0900

    [ etc ] 주석 추가

--- useRef_training/src/App.jsx
+++ useRef_training/src/App.jsx
@@ -1,15 +1,19 @@
 import WithState from "./Storage/WithState";
-import WithVar from "./Storage/WithVar";
-import RefUsing from "./Storage/RefUsing";
-import Login from "./DOM/Login";
+// import WithVar from "./Storage/WithVar";
+// import RefUsing from "./Storage/RefUsing";
+// import Login from "./DOM/Login";
+import { useRef } from "react";
 
 export default function App() {
+  const ref = useRef(0);
+
+  console.log(0);
   return (
     <>
       <WithState />
-      <WithVar />
+      {/* <WithVar />
       <RefUsing />
-      <Login />
+      <Login /> */}
     </>
   );
 }
